6ddb101d6495bd4b4dff1cb9587ff3bc56979b70
Justin W Hall <justinwhall@me.com>
2019-09-27 18:19:34 -0600

    example of installing publisher theme via NPM

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Scott Bolinger',
+    description: 'Headless WordPress with Gatsby FTW.',
+    author: 'Scott Bolinger',
+    twitter: '@scottbolinger',
+    siteUrl: `https://scottbolinger.com`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-publisher`,
+      options: {
+        menuName: `PRIMARY`,
+        mailChimpEndpoint: 0,
+        dynamicComments: 1,
+        gaTrackingId: 0,
+        wordPressUrl: `http://data.gatsby.develop`,
+        blogURI: '/blog'
+      },
+    },
+  ],
+}
